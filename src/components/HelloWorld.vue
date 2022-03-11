04a603fe4a2aa808979ea73db3eae6656a4e29cb
Alaa <alaa.soudan.96@gmail.com>
2022-03-11 15:56:25 +0200

    data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -477,10 +477,11 @@
         </v-btn>
          </v-row>
         <export-excel 
+        :data="[form, ...forms]"
         
     class   = "btn btn-default"
 :fields = "json_fields"
-    worksheet = "My Worksheet"
+   
     name  = "استمارة مستفيد.xls"
          
         >
